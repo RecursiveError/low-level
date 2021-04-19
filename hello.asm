348bf96da90178f7b1bdf2c02bac93c956b52940
RecursiveError <65524156+RecursiveError@users.noreply.github.com>
2021-04-18 22:15:16 -0300

    primeiro programa em ASM

---
+++ hello.asm
@@ -0,0 +1,23 @@
+section .data
+    msg: db 'ola mundo', 10
+    msg2: dw 'primeiro programa em asm', 10
+
+section .text
+global _start
+
+_start:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 10
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg2
+    mov rdx, 25
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
